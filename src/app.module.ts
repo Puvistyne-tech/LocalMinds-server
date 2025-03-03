7dd6acaa0bbb50a01e3b18db683184539f78511c
puvistyne <puvistien@gmail.com>
2025-03-03 14:10:18 +0100

    app module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,46 +1,31 @@
-import {Module, Logger} from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {MongooseModule} from '@nestjs/mongoose';
 import {PostsModule} from './post/post.module';
 import {CategoryModule} from './category/category.module';
-import {ConfigModule} from '@nestjs/config';
-import {UserModule} from './user/user.module';
-import {AuthModule} from './auth/auth.module';
+import {ConfigModule} from "@nestjs/config";
+import {UserModule} from "./user/user.module";
+import {AuthModule} from "./auth/auth.module";
 
 @Module({
     imports: [
-        // Load environment variables globally
         ConfigModule.forRoot({
             isGlobal: true,
+            // envFilePath: '.env',
         }),
-
-        // Configure MongoDB connection
         MongooseModule.forRootAsync({
-            useFactory: () => {
-                const logger = new Logger('MongooseModule');
-                const uri = process.env.MONGO_URI;
-
-                // Log the MONGO_URI for debugging
-                logger.log(`MONGO_URI: ${uri}`);
-
-                if (!uri) {
-                    throw new Error('MONGO_URI is not defined in environment variables.');
-                }
-
-                return {
-                    uri,
-                    useNewUrlParser: true,
-                    useUnifiedTopology: true,
-                };
-            },
+            useFactory: () => ({
+                uri: process.env.MONGO_URI,
+                dbName: process.env.DB_NAME,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }),
         }),
-
-        // Import feature modules
         UserModule,
         AuthModule,
         PostsModule,
-        // CategoryModule, // Uncomment if needed
+        // CategoryModule,
     ],
     controllers: [AppController],
     providers: [AppService],
